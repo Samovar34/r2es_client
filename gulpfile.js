46c4495d18b10727db8d9b6d1c7cc39485fb4c93
unscript <alexkh34@gmail.com>
2017-01-17 23:35:24 +0300

    gulp watch and build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp"),
-      scss = require("gulp-sass");
+      scss = require("gulp-sass"),
+      del  = require("del"),
+      browserSync = require("browser-sync").create();
 
 let path = {
     build: {
@@ -12,9 +14,9 @@ let path = {
 
     watch: {
         html: "src/*.html",
-        style: "src/style/*.scss",
+        style: "src/style/**/*.scss",
         js: "src/js/**/*.js",
-        img: "src/img/*.*",
+        img: "src/img/**/*.*",
         fonts: "src/fonts/**/*.*"
     },
 
@@ -24,15 +26,19 @@ let path = {
         js: "src/js/**/*.js",
         img: "src/img/*.*",
         fonts: "src/fonts/**/*.*"
-    }
+    },
+
+    del: "build/**/*"
 }
 
 // TASKS
 
+// ***** BUILD *****
+
 // build html
 gulp.task("build:html", () => {
     gulp.src(path.src.html)
-        .pipe(path.build.html);
+        .pipe(gulp.dest(path.build.html));
 });
 
 // build style
@@ -43,3 +49,99 @@ gulp.task("build:style", () => {
         //add minify
         .pipe(gulp.dest(path.build.style));
 });
+
+
+// build js
+gulp.task("build:js", () => {
+    gulp.src(path.src.js)
+        // file include
+        // uglify
+        .pipe(gulp.dest(path.build.js));
+});
+
+// copy images from src to build
+gulp.task("build:img", () => {
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img));
+});
+
+// copy fonts from src to build
+gulp.task("build:fonts", ()=> {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task("build", () => {
+    gulp.start("build:html");
+    gulp.start("build:style");
+    gulp.start("build:js");
+    gulp.start("build:img");
+    gulp.start("build:fonts");
+});
+
+// ***** WATCH HANDLERS *****
+
+gulp.task("watch:html", ["build:html"], (done) => {
+    browserSync.reload();
+    done();
+});
+
+gulp.task("watch:style", ["build:style"], (done) => {
+    browserSync.reload();
+    done();
+});
+
+gulp.task("watch:js", ["build:js"], (done) => {
+    browserSync.reload();
+    done(); 
+});
+
+gulp.task("watch:img", ["build:img"], (done) => {
+    browserSync.reload();
+    done();    
+});
+
+gulp.task("watch:fonts", ["build:fonts"], (done) => {
+    browserSync.reload();
+    done();    
+});
+
+
+// ***** SPECIAL *****
+
+// удалить содержимое папки для сборки
+gulp.task("del", () => {
+    return del([path.del]);
+});
+
+// удалить и собрать проект
+gulp.task("delAndBuild", ["del"], () => {
+    gulp.start("build");
+});
+
+// run server and watch
+gulp.task("serve", (done) => {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+
+    // enable watch
+    gulp.watch(path.watch.html, ["watch:html"]);
+    gulp.watch(path.watch.style, ["watch:style"]);
+    gulp.watch(path.watch.js, ["watch:js"]);
+    gulp.watch(path.watch.img, ["watch:img"]);
+    gulp.watch(path.watch.fonts, ["watch:fonts"]);
+
+    done();
+});
+
+
+// ***** DEFAULT *****
+
+gulp.task("default", ["delAndBuild"], () => {
+    gulp.start("serve");
+});
+
+
